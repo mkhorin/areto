e2e6514b7d4ebd4869c37f2428f622e45470cb00
mkhorin <maksimovichu@gmail.com>
2017-02-02 20:58:35 +1000

    Add JSON file helpers

--- helpers/FileHelper.js
+++ helpers/FileHelper.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 module.exports = class FileHelper {
 
+    // DIR 
+    
     static readDir (dir, handler, cb) {
         fs.readdir(dir, (err, files)=> {
             err ? cb(err) : async.eachSeries(files, handler, cb);
@@ -31,6 +33,22 @@ module.exports = class FileHelper {
             });
         }, err => {
             err ? cb(err) : fs.rmdir(dir, cb);
+        });
+    }
+    
+    // JSON
+
+    static isJsonExt (file) {
+        return path.extname(file).toLowerCase() === '.json';
+    }
+
+    static readJsonFile (file, cb) {
+        fs.readFile(file, (err, data)=> {
+            try {
+                err ? cb(err) : cb(null, JSON.parse(data));
+            } catch (err) {
+                cb(err);
+            }
         });
     }
 };
