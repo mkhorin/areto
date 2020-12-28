72b0f428198903e1b90b42950e41ab72de190e03
mkhorin <maksimovichu@gmail.com>
2020-12-28 20:30:38 +1000

    Refactor filter validator

--- validator/FilterValidator.js
+++ validator/FilterValidator.js
@@ -17,7 +17,7 @@ module.exports = class FilterValidator extends Base {
 
     constructor (config) {
         super({
-            filter: null,
+            method: null,
             skipOnEmpty: false,
             skipOnArray: false,
             separator: ',',
@@ -27,20 +27,20 @@ module.exports = class FilterValidator extends Base {
     }
 
     prepareFilter () {
-        let filter = this.filter;
-        if (filter === null) {
-            throw new Error('Filter property must be set');
+        let method = this.method;
+        if (method === null) {
+            throw new Error('Method property must be set');
         }
-        if (typeof filter === 'string') {
-            filter = `filter${StringHelper.toFirstUpperCase(filter)}`;
-            filter = this.constructor[filter];
-            if (typeof filter !== 'function') {
-                throw new Error(`Inline filter not found: ${this.filter}`);
+        if (typeof method === 'string') {
+            method = `filter${StringHelper.toFirstUpperCase(method)}`;
+            method = this.constructor[method];
+            if (typeof method !== 'function') {
+                throw new Error(`Filter not found: ${this.method}`);
             }
-        } else if (typeof filter !== 'function') {
-            throw new Error('Filter must be function');
+        } else if (typeof method !== 'function') {
+            throw new Error('Method must be string or function');
         }
-        this.filter = filter;
+        this.method = method;
     }
 
     async validateAttr (attr, model) {
@@ -48,20 +48,7 @@ module.exports = class FilterValidator extends Base {
         if (Array.isArray(value) && this.skipOnArray) {
             return;
         }
-        if (typeof this.filter === 'function') {
-            return this.executeFilter(value, attr, model);
-        }
-        if (value === null || value === undefined) {
-            return;
-        }
-        if (typeof value[this.filter] !== 'function') {
-            throw new Error(`Inline filter not found: ${this.filter} in ${value.constructor.name}`);
-        }
-        model.set(attr, await value[this.filter]());
-    }
-
-    async executeFilter (value, attr, model) {
-        const result = await this.filter(...arguments, this);
+        const result = await this.method(value, attr, model, this);
         result instanceof Message
             ? this.addError(model, attr, result)
             : model.set(attr, result);
